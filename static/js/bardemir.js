998d18065d55a65f6cc91277122cff0f7a81eed8
Ademir de Alvarenga Oliveira <ademirao@gmail.com>
2015-12-22 17:50:15 -0200

    m

--- static/js/bardemir.js
+++ static/js/bardemir.js
@@ -1,3 +1,7 @@
+APP_PROPERTIES = {
+  facebookToken: null,
+}
+
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -15,10 +19,51 @@ function loadBardemirApi() {
   gapi.client.load('bardemir', 'v1', function() { bardemirMain(); }, bardemir);
 }
 
+function MenuItem(name, onClick) {
+  this.name = name;
+  this.onClick = onClick;
+}
+
+function listPosts() {
+  gapi.client.bardemir.posts.list({"auth": APP_PROPERTIES.facebookToken}).execute(function(response) {
+      $("#main_frame").empty();
+      for (i in response.items) {
+        post_div = $("<div class=post>"+ response.items[i].title  + "</div>");
+        $("#main_frame").append(post_div);
+      }
+  });
+}
+
+function caronas() {
+  mainFrame = $("#main_frame");
+  mainFrame.empty();
+  need = $("<button>Quero</button>");
+  offer = $("<button>Ofereço</button>");
+  share = $("<button>Compartilhar Taxi</button>");
+  mainFrame.append(need);
+  mainFrame.append(offer);
+  mainFrame.append(share);
+}
+
+function decorateMenu(menuItem) {
+}
+
+MENU_ITEMS = [
+  new MenuItem('Eventos', null),
+  new MenuItem('Playlists', null),
+  new MenuItem('Caronas', caronas),
+  new MenuItem('G. Estudos', listPosts),
+]
+
+function setMenuSelection(i) {
+  $("#menu_items li").removeClass("selected");
+  $("#menu_items").children().eq(i).addClass("selected");
+}
+
 var bardemirMain = function () {
   $("#spinner").show();
-  facebook_token = getCookie("facebook_access_token");
-  if (facebook_token == null) {
+  APP_PROPERTIES.facebookToken = getCookie("facebook_access_token");
+  if (APP_PROPERTIES.facebookToken == null) {
     var login_frame = $('<a id="login_frame" href="https://www.facebook.com/dialog/oauth?client_id=433202543531394&redirect_uri=' + BARDEMIR_HOST +  '/facebook/login&scope=user_managed_groups,user_events">Allow Bardemir to see my facebook information</a>');
     login_frame.display = "none";
     login_frame.appendTo("#main_frame");
@@ -30,16 +75,18 @@ var bardemirMain = function () {
     $("#login_frame").remove();
   });
   $("#spinner").hide();
-  button = $("<button>List</button>");
-  button.click(function () {
-      gapi.client.bardemir.posts.list({"auth": facebook_token }).execute(function(response) {
-            for (i in response.items) {
-              post_div = $("<div class=post><pre>"+ response.items[i].title  + "</pre></div>");
-              $("#main_frame").append(post_div);
-            }
-          });
-  });
-  $("#main_frame").append(button);
+  
+  menuItems = $("#menu_items");
+  for (i in MENU_ITEMS) {
+    menuItem = MENU_ITEMS[i];
+    menuItemLi = $('<li>' + menuItem.name + '</li>');
+    (function(item, li) {
+        menuItemLi.click(function () {
+            setMenuSelection(li);
+            item.onClick();
+            })})(menuItem, i);
+    menuItems.append(menuItemLi);
+  }
 }
 
 $(document).ready(function() {
